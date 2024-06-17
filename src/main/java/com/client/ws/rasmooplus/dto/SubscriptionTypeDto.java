a9c8685553b976c570f0b89e8d9e5470caf40bb8
Geziel Bueno <gezielbueno1@gmail.com>
2024-06-17 15:24:38 -0300

    Handles exceptions to field validations

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -16,17 +16,17 @@ import java.math.BigDecimal;
 public class SubscriptionTypeDto {
     private Long id;
 
-    @NotBlank(message = "Name field cannot be null or empty")
-    @Size(min = 5, max = 30, message = "The Name field must be between 5 and 30 characters long")
+    @NotBlank(message = "cannot be null or empty")
+    @Size(min = 5, max = 30, message = "must be between 5 and 30 characters long")
     private String name;
 
-    @Max(value = 12, message = "accessMonths field cannot be greater than 12")
+    @Max(value = 12, message = "cannot be greater than 12")
     private Long accessMonths;
 
-    @NotNull(message = "Price field cannot be null")
+    @NotNull(message = "cannot be null")
     private BigDecimal price;
 
-    @NotBlank(message = "productKey field cannot be null or empty")
-    @Size(min = 5, max = 15, message = "The productKey field must be between 5 and 15 characters long")
+    @NotBlank(message = "cannot be null or empty")
+    @Size(min = 5, max = 15, message = "must be between 5 and 15 characters long")
     private String productkey;
 }
